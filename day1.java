6fcbeba5491ea9abd926ec322b177761d34985f3
ElizabethLeeWH <leewenhui98@gmail.com>
2023-10-24 23:49:06 +0800

    //First commit

--- day1.java
+++ day1.java
@@ -1,1 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner; //import the scanner class
 
+public class day1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+
+        Scanner item = new Scanner(System.in);   
+        ArrayList<String> myShoppingCart = new ArrayList<String>();
+        String prompt = item.next();
+        
+        System.out.println("Welcome to your shopping cart");
+
+        switch(prompt.trim().toLowerCase()){
+            case "list":
+                while (!myShoppingCart.isEmpty()){
+                    for (String element : myShoppingCart){
+                        System.out.println(element);                        
+                    }
+
+                    if (myShoppingCart.isEmpty()){
+                        System.out.println("Your cart is empty");
+                        break;
+                    }
+                    
+                }
+
+        }
+
+        
+
+
+        // boolean isUnique = true;
+        //     for (String i: myShoppingCart){
+        //         if (input.equals(myShoppingCart.get()){
+        //             isUnique = false;
+        //         }
+        //     }
+        //     if (isUnique == true){
+        //         myShoppingCart.add(input);
+        //     }
+    
+    }
+}
